25f53161407ddb82c11e237e120bac2a7ff54215
pablodecker <pablodecker@gmail.com>
2016-01-26 08:04:31 -0300

    probando recepcion de sms 2

--- app/src/main/java/com/decker/pablo/interfazportero2/MainActivity.java
+++ app/src/main/java/com/decker/pablo/interfazportero2/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,10 +20,12 @@ public class MainActivity extends AppCompatActivity {
     //cambio v1.3
     ListView lista;
     String[] stArreglo;
+    private String TAG = "Pablito";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "** Entro al Main");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         lista = (ListView)findViewById(R.id.listViewEquipos);
--- app/src/main/java/com/decker/pablo/interfazportero2/SMSReceiver.java
+++ app/src/main/java/com/decker/pablo/interfazportero2/SMSReceiver.java
@@ -15,16 +15,17 @@ import android.widget.Toast;
  */
 public class SMSReceiver extends BroadcastReceiver {
 
-    private String TAG = SMSReceiver.class.getSimpleName();
+    private String TAG = "Pablito";
 
     public SMSReceiver() {
     }
     public void onReceive(Context context, Intent intent)
     {
+        Log.d(TAG, "** Recibi algun SMS");
+        Toast.makeText(context, "Recibi Algo", Toast.LENGTH_SHORT).show();
         Bundle myBundle = intent.getExtras();
         SmsMessage [] messages = null;
         String strMessage = "";
-
         if (myBundle != null)
         {
             Object [] pdus = (Object[]) myBundle.get("pdus");
@@ -46,38 +47,9 @@ public class SMSReceiver extends BroadcastReceiver {
                 strMessage += "\n";
             }
 
-            Log.e("SMS", strMessage);
+            Log.d(TAG, strMessage);
             Toast.makeText(context, strMessage, Toast.LENGTH_SHORT).show();
         }
     }
-//    @Override
-//    public void onReceive(Context context, Intent intent) {
-//        // Get the data (SMS data) bound to intent
-//        Bundle bundle = intent.getExtras();
-//
-//        SmsMessage[] msgs = null;
-//
-//        String str = "";
-//
-//        if (bundle != null) {
-//            // Retrieve the SMS Messages received
-//            Object[] pdus = (Object[]) bundle.get("pdus");
-//            msgs = new SmsMessage[pdus.length];
-//
-//            // For every SMS message received
-//            for (int i=0; i < msgs.length; i++) {
-//                // Convert Object array
-//                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-//                // Sender's phone number
-//                str += "SMS from " + msgs[i].getOriginatingAddress() + " : ";
-//                // Fetch the text message
-//                str += msgs[i].getMessageBody().toString();
-//                // Newline <img src="http://codetheory.in/wp-includes/images/smilies/simple-smile.png" alt=":-)" class="wp-smiley" style="height: 1em; max-height: 1em;">
-//                str += "\n";
-//            }
-//
-//            // Display the entire SMS Message
-//            Log.d(TAG, str);
-//        }
-//    }
+
 }
